238084606f310a11d9236c1c895cd3789170501b
acay <iacayrin@gmail.com>
2021-03-16 18:25:41 +0700

    minor changes, ip lookup

--- _discordjs/Rimu.js
+++ _discordjs/Rimu.js
@@ -5,6 +5,7 @@ const {
 const config = require('./rimu/config');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const publicIp = require('public-ip');
 
 const revision = require('child_process').execSync(`git ls-remote https://${config.gitUser}:"${config.gitPass}"@github.com/Acayrin/acay-s-bot/ | head -1 | sed "s/HEAD//"`).toString().trim().substring(0, 10);
 
@@ -32,6 +33,13 @@ client.once('ready', () => {
   require('./rimu/task').task(client);
 
   require('./rimu/console').cmd(client);
+  (async () => {
+    console.log(await publicIp.v4());
+    //=> '46.5.21.123'
+
+    console.log(await publicIp.v6());
+    //=> 'fe80::200:f8ff:fe21:67cf'
+  })();
 });
 client.on('message', message => {
   require('./emotes/main').gproc(message);
